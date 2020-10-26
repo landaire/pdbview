1b57b84628c145a66737959e8f866d184d1489e0
Lander Brandt <landerbrandt@gmail.com>
2020-10-25 22:21:12 -0700

    Add JSON serialization

--- src/output.rs
+++ src/output.rs
@@ -6,5 +6,5 @@ pub fn print_plain(output: &mut impl Write, pdb_info: &ParsedPdb) -> io::Result<
 }
 
 pub fn print_json(output: &mut impl Write, pdb_info: &ParsedPdb) -> io::Result<()> {
-    Ok(())
+    write!(output, "{}", serde_json::to_string(pdb_info)?)
 }
